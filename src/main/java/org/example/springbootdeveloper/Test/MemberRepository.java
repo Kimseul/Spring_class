533db1323db6f999a457ceee0842ccb3da78667d
Kimseul <yims00224@gmail.com>
2023-09-25 16:10:34 +0900

    commit add 'TestController','Member','MemberRepository','TestService','datasql' edit 'application.yml' conect database

---
+++ src/main/java/org/example/springbootdeveloper/Test/MemberRepository.java
@@ -0,0 +1,6 @@
+package org.example.springbootdeveloper.Test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
