c950438486795a31778f5e8e4b893a24de310bd4
Kimseul <yims00224@gmail.com>
2023-10-16 16:26:09 +0900

    commit add 'TestControllerTest' study TestCode Pattern

--- src/main/java/org/example/springbootdeveloper/Test/Member.java
+++ src/main/java/org/example/springbootdeveloper/Test/Member.java
@@ -13,9 +13,8 @@ import lombok.NoArgsConstructor;
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column( name = "id", updatable = false)
+    @Column(name = "id", updatable = false)
     private Long id;
-
-    @Column( name = "name", nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 }
