3db0db3bd09d6439f3883e96e60bbdb07c2e3b17
Kimseul <yims00224@gmail.com>
2023-10-31 12:03:00 +0900

    commit delete 'JUnitTest/JUnitClcyeTest/Member/MemberRepository/TestController/TestControllerTest/TestService.java' add 'AddArticleRequest/Article/BlogApiController/BlogRepository/BlogService' new branch feature make example 'blog'

---
+++ src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,30 @@
+package org.example.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
+import org.example.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.example.springbootdeveloper.dto.AddArticleRequest;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article saveArticle = blogService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(saveArticle);
+
+    }
+
+
+}
